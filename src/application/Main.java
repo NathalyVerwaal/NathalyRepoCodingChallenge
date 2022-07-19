20e2037e992d22826cb81f98ce56c64ce4ae0b7a
matthew.buhler <matthew.buhler@IC-CS233-26.uc.ucalgary.ca>
2022-07-19 12:13:48 -0600

    Changed label to Matthew

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
-			root.getChildren().add(new Label("Sydney's label"));
+			root.getChildren().add(new Label("Matthew's label"));
 
 			root.getChildren().add(new Label("Sydney branch"));
 
